5ed1aaae42892ad2a6d4110510b523d371a8e227
thomas zangari <tomsa78@hotmail.it>
2019-07-19 13:26:22 -0500

    update

---
+++ MyShop.Web.Ui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.Web.Ui.Startup))]
+namespace MyShop.Web.Ui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
